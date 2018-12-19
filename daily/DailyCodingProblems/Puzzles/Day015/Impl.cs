a2ae0a21e68472b5942385dec334451a9b7ae4ac
Eoin Campbell <eoin.campbell@channelsight.com>
2018-12-19 09:43:19 +0000

    DCP Day 15

--- daily/DailyCodingProblems/Puzzles/Day015/Impl.cs
+++ daily/DailyCodingProblems/Puzzles/Day015/Impl.cs
@@ -7,6 +7,24 @@
     using System.Text.RegularExpressions;
     using System.Threading.Tasks;
 
+    public static class Day15Extensions
+    {
+        public static T GetRandom<T>(this IEnumerable<T> source)
+        {
+            var rand = new Random();
+            var count = 0;
+            var result = default(T);
+
+            foreach (var element in source)
+            {
+                if (count == 0) result = element;
+                else if (rand.Next(count) == count - 1) result = element;
+                count++;
+            }
+            return result;
+        }
+    }
+
     public class Impl : BasePuzzle
     {
         public Impl() : base("Day 004") { }
@@ -14,11 +32,10 @@
         protected override async Task<string> ExecuteImpl()
         {
             var rand = new Random();
-            var length = 100;
-            var attempts = 100000;
+            const int length = 100;
+            const double attempts = 100000;
 
             //Test 1
-
             var list = Generator(length).ToList();
             var test1Results = new Dictionary<int, int>();
             for (var i = 0; i < attempts; i++)
@@ -28,25 +45,19 @@
                 else test1Results[r1]++;
             }
             foreach(var t1 in test1Results.Keys)
-                Console.WriteLine($"{t1} - {test1Results[t1]:000000} - {((double)test1Results[t1] / (double)attempts):P}");
-            
+                Console.WriteLine($"{t1} - {test1Results[t1]:000000} - {(test1Results[t1]/attempts):P}");
+
 
             //Test2
-            int length2 = 1000000, count = 1, r2 = 0;
             var test2Results = new Dictionary<int, int>();
-
             for (var i = 0; i < attempts; i++)
             {
-                r2 = GetRandomFromStream(Generator(length), rand);
-
+                var r2 = Generator(length).GetRandom();
                 if (!test2Results.ContainsKey(r2)) test2Results.Add(r2, 1);
                 else test2Results[r2]++;
             }
-
-
-
             foreach (var t2 in test2Results.Keys)
-                Console.WriteLine($"{t2} - {test2Results[t2]:000000} - {((double)test2Results[t2] / (double)attempts):P}");
+                Console.WriteLine($"{t2} - {test2Results[t2]:000000} - {(test2Results[t2]/attempts):P}");
 
 
             return await Task.FromResult($"");
@@ -57,28 +68,7 @@
 
         }
 
-        private int GetRandomFromStream(IEnumerable<int> stream, Random rand)
-        {
-            int count = 0, r2 = 0;
-            foreach (var i in stream)
-            {
-
-                if (count == 0) // Always select the first element
-                {
-                    r2 = i;
-                }
-                else
-                {
-                    // Replace the previous random item with a new item with 1/count probability
-                    var rx = rand.Next(0, count);
-                    if (rx == count - 1)
-                        r2 = i;
-                }
-
-                count++;
-            }
-            return r2;
-        }
+        
 
         private IEnumerable<int> Generator(int n)
         {
