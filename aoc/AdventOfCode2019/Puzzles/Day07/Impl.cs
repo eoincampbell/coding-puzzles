9beff71e994d52eaf19fddf1fe49543c8c31a335
Eoin Campbell <eoin.campbell@gmail.com>
2019-12-07 11:55:28 +0000

    Day 7 - Cleaned up

--- aoc/AdventOfCode2019/Puzzles/Day07/Impl.cs
+++ aoc/AdventOfCode2019/Puzzles/Day07/Impl.cs
@@ -2,18 +2,16 @@
  * Day 07: Amplification Circuit
  * https://adventofcode.com/2019/day/7
  * Part 1: 22012
- * Part 2: 
+ * Part 2: 4039164
  */
 namespace AdventOfCode2019.Puzzles.Day07
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
     using Base;
     using Combinatorics.Collections;
     
-
     public class Impl : BasePuzzle<string, int>
     {
         public Impl() : base("Day 07: Amplification Circuit", ".\\Puzzles\\Day07\\Input.txt") { }
@@ -42,48 +40,53 @@ namespace AdventOfCode2019.Puzzles.Day07
 
         public override async Task<int> RunPart2Async()
         {
-            //return 0;
             var c = new Permutations<int>(new int[] { 5, 6, 7, 8, 9 }, GenerateOption.WithoutRepetition);
             var bestOutput = 0;
 
             foreach (var phaseSettings in c)
             {
-                var aInputs = new Queue<int>(new int[] { phaseSettings[0], 0 });
-                var bInputs = new Queue<int>(new int[] { phaseSettings[1]  });
-                var cInputs = new Queue<int>(new int[] { phaseSettings[2]  });
-                var dInputs = new Queue<int>(new int[] { phaseSettings[3]  });
-                var eInputs = new Queue<int>(new int[] { phaseSettings[4]  });
-
-
-                var aVm = new IntCodeVm(Inputs[0], aInputs);
-                var bVm = new IntCodeVm(Inputs[0], bInputs);
-                var cVm = new IntCodeVm(Inputs[0], cInputs);
-                var dVm = new IntCodeVm(Inputs[0], dInputs);
-                var eVm = new IntCodeVm(Inputs[0], eInputs);
-
+                var inputs = GetInputs(phaseSettings);
+                var vms = GetVms(inputs);
                 var result = 0;
 
-                while (!aVm.IsHalted)
+                while (!vms[0].IsHalted)
                 {
-                    var aOuput = aVm.RunProgramPauseAtOutput();
-                    bInputs.Enqueue(aOuput);
-                    var bOuput = bVm.RunProgramPauseAtOutput();
-                    cInputs.Enqueue(bOuput);
-                    var cOuput = cVm.RunProgramPauseAtOutput();
-                    dInputs.Enqueue(cOuput);
-                    var dOuput = dVm.RunProgramPauseAtOutput();
-                    eInputs.Enqueue(dOuput);
-                    var eOuput = eVm.RunProgramPauseAtOutput();
-
-                    if (aVm.IsHalted) result = eOuput;
-                    else aInputs.Enqueue(eOuput);
-                    
+                    for (int i = 0; i < 5; i++)
+                    {
+                        var nextOutput = vms[i].RunProgramPauseAtOutput();
+                        if (i == 4 && vms[0].IsHalted)
+                            result = nextOutput;
+                        else
+                        {
+                            var nextInputIdx = i + 1 == 5 ? 0 : i + 1;
+                            inputs[nextInputIdx].Enqueue(nextOutput);
+                        }
+                    }
                 }
                 
                 if (result > bestOutput)
                     bestOutput = result;
             }
             return bestOutput;
+        }
+
+        private IList<IntCodeVm> GetVms(IList<Queue<int>> inputs) 
+            => Enumerable.Range(0, 5)
+                    .Select(r => new IntCodeVm(Inputs[0], inputs[r]))
+                    .ToList();
+
+        private IList<Queue<int>> GetInputs(IList<int> phaseSettings)
+        {
+            var inputs = new List<Queue<int>>
+                {
+                    new Queue<int>(new int[] { phaseSettings[0], 0 })
+                };
+            inputs.AddRange(
+                Enumerable.Range(1, 4)
+                .Select(r => new Queue<int>(new int[] { phaseSettings[r] }))
+                );
+
+            return inputs;
         }
     }
 }
