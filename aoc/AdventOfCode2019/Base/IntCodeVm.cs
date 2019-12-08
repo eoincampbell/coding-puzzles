0801e78d43efd42c6719751d2905551d0052b048
Eoin Campbell <eoin.campbell@gmail.com>
2019-12-08 00:38:36 +0000

    refactored to store tape in a statemachine that can be operated on

--- aoc/AdventOfCode2019/Base/IntCodeVm.cs
+++ aoc/AdventOfCode2019/Base/IntCodeVm.cs
@@ -38,35 +38,62 @@
         public Func<Mode[], int[], int> Execute { get; }
     }
 
-    public class StateMachines
+    public class StateMachine
     {
-        public StateMachines(int[] tape)
-        {
+        private readonly int[] _tape;
+        public int Pointer { get; private set; }
 
+        public StateMachine(int[] tape)
+        {
+            _tape = tape;
+        }
+
+        public int GetValue() 
+            => _tape[Pointer];
+
+        public void SetPointer(int pointer) 
+            => Pointer = pointer;
+
+        public void SetValue(int pointer, int value) 
+            => _tape[pointer] = value;
+
+        public int GetValue(int pointer)
+            => _tape[pointer];
+
+        public int GetValueOffset(int offset, Mode mode)
+            => (mode == Mode.Position)
+                ? _tape[_tape[Pointer + offset]]
+                : _tape[Pointer + offset];
+
+        public void SetValueOffset(int offset, Mode[] modes, int value)
+        {
+            var idx = (modes[offset - 1] == 0)
+                ? _tape[Pointer + offset]
+                : Pointer + offset;
+            _tape[idx] = value;
         }
     }
 
     public class IntCodeVm
     {
-        private readonly int[] _tape;
-        private Queue<int> Inputs { get; set; }
-        private Queue<int> Outputs { get; set; }
+        private readonly StateMachine _stateMachine;
+        private readonly Queue<int> _inputs;
+        private readonly Queue<int> _outputs; 
         private int _lastOutput;
         private readonly CommandDict _commands;
         public Action<string> LogAction;
 
         public bool IsHalted { get; private set; }
         public bool IsPaused { get; set; }
-        public int Pointer { get; private set; }
 
         public IntCodeVm(string tape)
             : this(Array.ConvertAll(tape.Split(','), int.Parse)) { }
 
         public IntCodeVm(int [] tape)
         {
-            _tape = tape;
-            Inputs = new Queue<int>();
-            Outputs = new Queue<int>();
+            _stateMachine = new StateMachine( tape);
+            _inputs = new Queue<int>();
+            _outputs = new Queue<int>();
             _commands = new CommandDict
             {
                 {01, new VmCommand(01, "ADD", 2, 4, Add)},
@@ -100,21 +127,20 @@
 
         private void ProcessNextCommand()
         {
-            var poc = _tape[Pointer];
+            var poc = _stateMachine.GetValue();
             var opCode = poc % 100;
             var modes = GetModes(poc);
             var c = _commands[opCode];
             var param = GetParams(c.InputCount, modes);
             var pointer = c.Execute(modes, param);
-            LogAction?.Invoke($"{opCode:00} {Pointer:0000} {c.Execute.Method.Name:10} {string.Join(" ",param)}");
-            Pointer = pointer + c.MovePtrForward;
+            LogAction?.Invoke($"{opCode:00} {_stateMachine.Pointer:0000} {c.Execute.Method.Name:10} {string.Join(" ",param)}");
+            _stateMachine.SetPointer( pointer + c.MovePtrForward);
         }
 
-        public void AddInput(int input) => Inputs.Enqueue(input);
+        public void AddInput(int input) => _inputs.Enqueue(input);
         public int GetOutput()
         {
-            int temp;
-            if (Outputs.TryDequeue(out temp)) 
+            if (_outputs.TryDequeue(out int temp))
                 _lastOutput = temp;
 
             return _lastOutput;
@@ -122,7 +148,7 @@
 
         public IEnumerable<int> GetOutputs()
         {
-            var o = Outputs.ToList();
+            var o = _outputs.ToList();
             _lastOutput = o.Last();
             return o;
         }
@@ -138,86 +164,74 @@
         private int[] GetParams(int count, Mode[] modes)
             => Enumerable
                 .Range(1, count)
-                .Select(i => GetValueOffset(i, modes[i - 1]))
+                .Select(i => _stateMachine.GetValueOffset(i, modes[i - 1]))
                 .ToArray();
 
         public void SetValue(int pointer, int value)
-            => _tape[pointer] = value;
+            => _stateMachine.SetValue(pointer, value);
 
         public int GetValue(int pointer) 
-            =>_tape[pointer]; 
+            => _stateMachine.GetValue(pointer); 
 
-        private int GetValueOffset(int offset, Mode mode)
-            => (mode == Mode.Position)
-                ? _tape[_tape[Pointer + offset]]
-                : _tape[Pointer + offset];
-
-        private void SetValueOffset(int offset, Mode[] modes, int value)
-        {
-            var idx = (modes[offset - 1] == 0)
-                ? _tape[Pointer + offset]
-                : Pointer + offset;
-            _tape[idx] = value;
-        }
 
         #region command implementations 
         private int Add(Mode[] modes, int[] p)
         {
-            SetValueOffset(3, modes, p[0] + p[1]);
-            return Pointer;
+            _stateMachine.SetValueOffset(3, modes, p[0] + p[1]);
+            return _stateMachine.Pointer;
         }
 
         private int Mul(Mode[] modes, int[] p)
         {
-            SetValueOffset(3, modes, p[0] * p[1]);
-            return Pointer;
+            _stateMachine.SetValueOffset(3, modes, p[0] * p[1]);
+            return _stateMachine.Pointer;
         }
 
         private int Input(Mode[] modes, int[] p)
         {
-            SetValueOffset(1, modes, Inputs.Dequeue());
-            return Pointer;
+            _stateMachine.SetValueOffset(1, modes, _inputs.Dequeue());
+            return _stateMachine.Pointer;
         }
 
         private int Output(Mode[] modes, int[] p)
         {
-            Outputs.Enqueue(p[0]);
+            _outputs.Enqueue(p[0]);
             IsPaused = true;
-            return Pointer;
+            return _stateMachine.Pointer;
         }
 
         private int JumpIfTrue(Mode[] modes, int[] p)
         {
             return (p[0] != 0)
                 ? p[1]
-                : Pointer + 3;
+                : _stateMachine.Pointer + 3;
         }
 
         private int JumpIfFalse(Mode[] modes, int[] p)
         {
             return (p[0] == 0)
                 ? p[1]
-                : Pointer + 3;
+                : _stateMachine.Pointer + 3;
         }
 
         private int LessThan(Mode[] modes, int[] p)
         {
-            SetValueOffset(3, modes, (p[0] < p[1]) ? 1 : 0);
-            return Pointer;
+            _stateMachine.SetValueOffset(3, modes, (p[0] < p[1]) ? 1 : 0);
+            return _stateMachine.Pointer;
         }
 
         private int EqualTo(Mode[] modes, int[] p)
         {
-            SetValueOffset(3, modes, (p[0] == p[1]) ? 1 : 0);
-            return Pointer;
+            _stateMachine.SetValueOffset(3, modes, (p[0] == p[1]) ? 1 : 0);
+            return _stateMachine.Pointer;
         }
 
         private int Halt(Mode[] modes, int[] p)
         {
             IsHalted = true;
-            return Pointer;
+            return _stateMachine.Pointer;
         }
 
-            #endregion             
+        #endregion             
     }
 }
