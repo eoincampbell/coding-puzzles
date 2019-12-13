0056f83d1b3ea7e3c9a6e3d32dbcd80684d5833c
Eoin Campbell <eoin.campbell@channelsight.com>
2019-12-13 17:29:36 +0000

    Cleaned up

--- aoc/AdventOfCode2019.Tests/PuzzleTests1115.cs
+++ aoc/AdventOfCode2019.Tests/PuzzleTests1115.cs
@@ -96,7 +96,7 @@ namespace AdventOfCode2019.Tests
             var p = new Puzzles.Day13.Impl();
             await p.ResetInputsAsync();
             var a = await p.RunPart2Async();
-            Assert.AreEqual(0, a);
+            Assert.AreEqual(17138, a);
         }
 
         [Test]
--- aoc/AdventOfCode2019/Puzzles/Day13/Impl.cs
+++ aoc/AdventOfCode2019/Puzzles/Day13/Impl.cs
@@ -1,16 +1,14 @@
 ﻿/*
  * Day 13: 
  * https://adventofcode.com/2019/day/13
- * Part 1: 
- * Part 2: 
+ * Part 1: 341
+ * Part 2: 17138
  */
 namespace AdventOfCode2019.Puzzles.Day13
 {
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Numerics;
-    using System.Threading;
     using System.Threading.Tasks;
     using Base;
     using Base.IntCode;
@@ -31,116 +29,138 @@ namespace AdventOfCode2019.Puzzles.Day13
             {4, BALL}
         };
 
-        private int _maxX;
-        private int _maxY;
+        private (int x, int y) _max;
         private int _blocks;
         private int _score;
         private IntCodeVm _vm;
-        private int _ballPositionX;
-        private int _ballPositionY;
-        private int _ballPrevPositionX;
-        private int _ballPrevPositionY;
+        private VmState _state;
+        private (int x, int y) _ballPos;
+        private (int x, int y) _ballPrevPos;
         private int _paddlePositionX;
-
+        private bool _playAreaRendered;
+        private readonly bool _render;
         private Dictionary<(int x, int y), char> _tiles = new Dictionary<(int x, int y), char>();
 
-
-        public Impl() : base("Day 13: ", ".\\Puzzles\\Day13\\Input.txt") { }
+        public Impl() : base("Day 13: ", ".\\Puzzles\\Day13\\Input.txt") => _render = false;
+        public Impl(bool render) : base("Day 13: ", ".\\Puzzles\\Day13\\Input.txt") => _render = render;
 
         public override async Task<int> RunPart1Async()
              => await Task.Run(() =>
              {
                  ResetVm();
-
                  _vm.RunProgramUntilHalt();
-
-                 var results = _vm.GetOutputs().ToList();
-
-                 for (var i = 0; i < results.Count; i+=3)
-                 {
-                     var x = (int)results[i];
-                     var y = (int)results[i+1];
-                     var t = TileCodes[(int) results[i + 2]];
-                     _tiles.Add((x, y), t);
-
-                     if(x > _maxX) _maxX = x;
-                     if(y > _maxY) _maxY = y; 
-                     if(t == BLOK) _blocks++;
-                 }
-
+                 ProcessAnyOutputs();
                  RenderOutput();
-                 
-
+                 SetFinalCursorPosition(2);
                  return _blocks;
              });
 
         public override async Task<int> RunPart2Async()
         {
-            //ResetVm();
-            //_vm.SetValue(0, 2);
+            ResetVm();
+            _vm.SetValue(0, 2);
+            while (_state != VmState.Halted)
+            {
+                _state = _vm.RunProgramUntilInputRequired();
+                if (_state == VmState.Halted)
+                {
+                    ProcessAnyOutputs();
+                    break;
+                }
 
-            //var state = VmState.Running;
+                if (_state != VmState.PausedAwaitingInput) continue;
+                //Output is called twice, once to show you the current state of the ball
+                //Then wait for input
+                //Then again to show you the updated position of your paddle.
+                //So only go into the InputCommandCode if the update included a ball-position updated
+                var ballMoved = ProcessAnyOutputs();
+                RenderOutput();
+                if (ballMoved)
+                    ProcessInputCommand();
 
-            //while (state != VmState.Halted)
-            //{
-            //    state = _vm.RunProgramUntilInputRequired();
+                await Wait();
+            }
+            SetFinalCursorPosition(3);
+            return _score;
+        }
 
-            //    if (state == VmState.Halted)
-            //    {
-            //        ProcessAnyOutputs();
-            //        break;
-            //    }
-
-            //    if (state != VmState.PausedAwaitingInput) continue;
-
-            //    ProcessAnyOutputs();
-            //    RenderOutput();
-            //    ProcessInputCommand();
-
-            //    await Task.Delay(2000);
-
-            //}
-
-            return _blocks; //_score;
+        private void SetFinalCursorPosition(int i)
+        {
+            if(_render)
+                Console.SetCursorPosition(0, _max.y + i);
         }
 
         private void ProcessInputCommand()
         {
-            //Out of order logic.
-
             //The picture on the ball on screen is as of the last output.
             //Therefore any input I supply here needs to put the paddle where the ball is going to be.
+            //use ballPositionY to figure out if the ball is immediately above the cursor and about to bounce away... don't do anything until you know the bounce direction.
+            //otherwise
+            //      use prev vs. current x position of ball to figure out ball direction U/D/L/R
+            //      take into account a wall bounce to modify the direction
+            //use the direction to predict a next position and therefore modify the paddle +/-1;
 
-            //use prev vs. current vars to figure out ball direction U/D/L/R
-            //use paddle+/-1 versus ball-x to check where I need to be.
+            var dir = (_ballPos.y == _max.y - 2)
+                ? 0
+                : (_ballPrevPos.x == 0 || _ballPos.x > _ballPrevPos.x) 
+                    ? 1 
+                    : -1;
+            
+            if (_ballPos.x + 1 == _max.x || _ballPos.x - 1 == 0) //at the wall
+                dir *= -1;
+
+            var next = _ballPos.x + dir;
+
+
+            if (_paddlePositionX < next)
+            {
+                _vm.SetInput(1);
+                _paddlePositionX++;
+            }
+            else if(_paddlePositionX > next)
+            {
+                _vm.SetInput(-1);
+                _paddlePositionX--;
+            }
+            else
+            {
+                _vm.SetInput(0);
+            }
         }
 
         private void ResetVm()
         {
             _vm = new IntCodeVm(Inputs[0]);
+            _state = _vm.State;
             _tiles = new Dictionary<(int x, int y), char>();
-            _maxX = 0;
-            _maxY = 0;
+            _max = (0, 0);
             _blocks = 0;
-            _ballPositionX = 0;
-            _ballPrevPositionX = 0;
-            _ballPositionY = 0;
-            _ballPrevPositionY = 0;
+            _ballPos = (0,0);
+            _ballPrevPos = (0,0);
             _paddlePositionX = 0;
             _score = 0;
+            _playAreaRendered= false;
         }
 
-        private void ProcessAnyOutputs()
+        private bool ProcessAnyOutputs()
         {
-            if (!_vm.HasOutputs) return;
+            var ballMoved = false;
+            if (!_vm.HasOutputs) return false;
 
             var results = _vm.GetOutputs().ToList();
 
+            _tiles.Clear();
             for (var i = 0; i < results.Count; i += 3)
             {
                 if (results[i] == -1 && results[i + 1] == 0)
                 {
                     _score = (int)results[i + 2];
+
+                    if (_render)
+                    {
+                        Console.SetCursorPosition(0, _max.y + 1);
+                        Console.WriteLine($"Score: {_score}");
+                    }
                 }
                 else
                 {
@@ -148,45 +168,54 @@ namespace AdventOfCode2019.Puzzles.Day13
                     var y = (int) results[i + 1];
                     var t = TileCodes[(int) results[i + 2]];
 
-                    if (x > _maxX) _maxX = x;
-                    if (y > _maxY) _maxY = y;
+                    if (x > _max.x) _max.x = x;
+                    if (y > _max.y) _max.y = y;
 
                     if (t == BLOK) _blocks++;
                     if (t == PADL) _paddlePositionX = x;
-
                     if (t == BALL)
                     {
-                        _ballPrevPositionX = _ballPositionX;
-                        _ballPrevPositionY = _ballPositionY;
-                        _ballPositionX = x;
-                        _ballPositionY = y;
+                        ballMoved = true;
+                        _ballPrevPos = _ballPos;
+                        _ballPos.x = x;
+                        _ballPos.y = y;
                     }
 
                     if (_tiles.ContainsKey((x, y)))
-                    {
                         _tiles[(x,y)] = t;
-                    }
                     else
-                    {
                         _tiles.Add((x, y), t);
-                    }
-
                 }
-
-                
             }
+            return ballMoved;
+        }
+
+        private async Task Wait() => await Task.Delay(_render ? 5 : 0);
+
+        private void RenderPlayArea()
+        {
+            if (_playAreaRendered) return;
+            Console.SetCursorPosition(0, 0);
+            for (var yy = 0; yy <= _max.y; yy++)
+            for (var xx = 0; xx <= _max.x; xx++)
+            {
+                Console.Write(" ");
+                if (xx == _max.x) Console.WriteLine("");
+            }
+
+            _playAreaRendered = true;
         }
 
         private void RenderOutput()
         {
-            if(Console.IsOutputRedirected) return;
-
+            if(!_render) return;
+            RenderPlayArea();
             Console.SetCursorPosition(0, 0);
-            for (var yy = 0; yy <= _maxY; yy++)
-            for (var xx = 0; xx <= _maxX; xx++)
+            foreach (var key in _tiles.Keys)
             {
-                Console.Write(_tiles.ContainsKey((xx, yy)) ? _tiles[(xx,yy)] : ' ');
-                if (xx == _maxX) Console.WriteLine("");
+                var t = _tiles[key];
+                Console.SetCursorPosition(key.x, key.y);
+                Console.Write(t);
             }
         }
     }
