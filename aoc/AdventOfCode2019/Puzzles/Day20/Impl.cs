304631c050e9a7303c7b11d166d24f98d49d7f44
Eoin Campbell <eoin.campbell@channelsight.com>
2019-12-20 13:12:07 +0000

    Day 20 Pt2WIP

--- aoc/AdventOfCode2019/Puzzles/Day20/Impl.cs
+++ aoc/AdventOfCode2019/Puzzles/Day20/Impl.cs
@@ -21,13 +21,20 @@ namespace AdventOfCode2019.Puzzles.Day20
         public Impl() : base("Day 20: ", ".\\Puzzles\\Day20\\Input.txt")
         {
             _c = new Dictionary<(int x, int y),  (string portal, bool outward)>();
+            _rC = new Dictionary<(int x, int y), List<(int x, int y, int lvl)>();
             _graph = new Graph();
             _rGraph = new RGraph();
+
+            _height = Inputs.Count;
+            _width = Inputs[0].Length;
         }
 
         private readonly Dictionary<(int x, int y), (string portal, bool outward)> _c;
+        private readonly Dictionary<(int x, int y), List<(int x, int y, int lvl)>> _rC;
         private readonly Graph _graph;
         private readonly RGraph _rGraph;
+        private int _width = 0;
+        private int _height = 0;
 
         public override async Task<int> RunPart1Async() => await Task.Run(() =>
         {
@@ -74,8 +81,14 @@ namespace AdventOfCode2019.Puzzles.Day20
                         if(Inputs[y+yy][x+xx] == '.')
                             _graph.AddVerticesAndEdge(new Edge<(int x, int y)>((x, y), (x+xx, y+yy)));
 
-                    if (!_c.ContainsKey((x, y)) || string.IsNullOrEmpty(_c[(x, y)].portal) || _c[(x, y)].portal == "AA" || _c[(x, y)].portal == "ZZ") continue;
+                    if (!_c.ContainsKey((x, y))
+                        || string.IsNullOrEmpty(_c[(x, y)].portal) 
+                        || _c[(x, y)].portal == "AA" 
+                        || _c[(x, y)].portal == "ZZ") 
+                        continue;
+
                     var o = _c.FirstOrDefault(w => w.Value == _c[(x, y)] && w.Key != (x, y));
+
                     _graph.AddVerticesAndEdge(new Edge<(int x, int y)>((x,y), o.Key));
                 }
         }
@@ -103,22 +116,44 @@ namespace AdventOfCode2019.Puzzles.Day20
             for (var y = 0; y < Inputs.Count; y++)
             for (var x = 0; x < Inputs[y].Length; x++)
                 if (Inputs[y][x] == '.')
+                {
+
+
                     _c.Add((x, y), (CheckForPortals(x, y), false));
+                }
         }
 
-        private string CheckForPortals(int x, int y)
-        { 
+        private (string portal, bool? outward) CheckForPortals(int x, int y)
+        {
+            string portal = string.Empty;
+            bool? outward = null;
             //north
-            if (IsLetter(Inputs[y - 1][x])) return GetPortal(x, y - 2, x, y - 1); 
+            if (IsLetter(Inputs[y - 1][x]))
+            {
+                portal = GetPortal(x, y - 2, x, y - 1);
+                outward = (y == 2);
+            } 
             //south
-            if (IsLetter(Inputs[y + 1][x])) return GetPortal(x, y + 1, x, y + 2);
+            if (IsLetter(Inputs[y + 1][x]))
+            {
+                portal = GetPortal(x, y + 1, x, y + 2);
+                outward = (y == _height - 2);
+            }
             //east
-            if (IsLetter(Inputs[y][x + 1])) return GetPortal(x +1, y, x + 2, y );
+            if (IsLetter(Inputs[y][x + 1]))
+            {
+                portal = GetPortal(x +1, y, x + 2, y );
+                outward = (x == _width - 2);
+            }
             //west
-            if (IsLetter(Inputs[y][x - 1])) return GetPortal(x - 2, y, x - 1, y);
-            
+            if (IsLetter(Inputs[y][x - 1]))
+            {
+                portal = GetPortal(x - 2, y, x - 1, y);
+                outward = (x == 2);
+            }
 
-            return string.Empty;
+
+            return (portal, outward);
         }
 
         public string GetPortal(int x1, int y1, int x2, int y2) => string.Join("", Inputs[y1][x1], Inputs[y2][x2]);
